e127d44fa54abd2bc87a5ef1d9b271076e4e3874
Gabriel Rivas <55334104+Gabriel-R285@users.noreply.github.com>
2025-06-27 21:37:10 +0000

    "commit"

--- CollegeERP/urls.py
+++ CollegeERP/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
+
+class LogoutGetView(LogoutView):
+    # permitimos explícitamente ambos métodos
+    http_method_names = ['get', 'post']
+
+    def get(self, request, *args, **kwargs):
+        # aquí llamamos al post() original para hacer el logout
+        return super().post(request, *args, **kwargs)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +20,5 @@ urlpatterns = [
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
     path('accounts/logout/',
-         auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+               LogoutGetView.as_view(template_name='info/logout.html'), name='logout'),
 ]
